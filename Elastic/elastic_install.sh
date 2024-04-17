f4e042486734722b26509bce5cfbe910c2f6e050
brandonntb <40216127+brandonntb@users.noreply.github.com>
2024-04-17 14:14:38 +0100

    Update elastic_install.sh

--- Elastic/elastic_install.sh
+++ Elastic/elastic_install.sh
@@ -3,20 +3,13 @@
 echo -e "Installing support packages ...\n"
 (sudo apt update && sudo apt install openjdk-8-jdk -y && sudo apt install nginx -y && sudo apt install apt-transport-https -y && sudo apt install curl -y) >/dev/null 2>&1
 
+# Adding elastic to the repo list
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
-
-
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 
 # Install elastic and save the creds
 echo -e "Installing Elastic ...\n"
-sudo apt update >/dev/null 2>&1 && sudo apt install elasticsearch -y > elasticinfo.txt
-echo "username:elastic" > elastic_user.txt
-sudo grep "superuser is :" elasticinfo.txt | sudo cut -d ":" -f 2 |tr -d ' ' >> elastic_pass.txt
-
-# Install and start SSH
-echo -e "Installing and starting SSH ...\n"
-(sudo apt install ssh -y && sudo systemctl start ssh) >/dev/null 2>&1
+(sudo apt update && sudo apt install elasticsearch -y) >/dev/null 2>&1
 
 # --- yml changes
 echo "Uncommenting 'network.host', changing to machines IP. Uncommenting 'http.port', change to '9200'."
@@ -26,8 +19,7 @@ sudo sed -i 's/#http.port: 9200/http.port: 9200/' /etc/elasticsearch/elasticsear
 echo "Adding transport.port"
 sudo echo "transport.port: 9310" >> /etc/elasticsearch/elasticsearch.yml
 
-# ---------- yml changes end
-
+# yml changes needed for certificate communication stuff
 echo "Adding '-Xms512m' and '-Xmx512m' as seperate lines in page"
 sudo echo -e "-Xms512m\n-Xmx512m" >> /etc/elasticsearch/jvm.options
 
@@ -44,10 +36,28 @@ for tkn in $(sudo /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-t
 echo -e "Restarting Kibana ...\n"
 sudo systemctl restart kibana
 
-# END
+# Create the certs and move them into the appropriate dirs
+echo -e "\n\n" | sudo /usr/share/elasticsearch/bin/elasticsearch-certutil ca
+echo -e "\n\n\n" | sudo /usr/share/elasticsearch/bin/elasticsearch-certutil cert --ca elastic-stack-ca.p12
+sudo cp /usr/share/elasticsearch/elastic-certificates.p12 /etc/elasticsearch/certs/
+sudo chown elasticsearch: /etc/elasticsearch/certs/elastic-certificates.p12
 
+# Add all the SSL security stuff the the ymls -- for certs || adds the path the the config options and sets up th eneeded parameters for certs
+sudo sed -i '/^xpack.security.transport.ssl:/,/^\s*$/ {/^\s*keystore\.path:/ s/.*/  keystore.path: certs\/elastic-certificates.p12/; /^\s*truststore\.path:/ s/.*/  truststore.path: certs\/elastic-certificates.p12/; s/^\(\s*verification_mode:\).*$/\1 certificate\n  client_authentication: required/;}' /etc/elasticsearch/elasticsearch.yml
+
+# Set the password for the certs to null/nothing
+echo ""
+echo -e "y\n\n" | sudo /usr/share/elasticsearch/bin/elasticsearch-keystore add xpack.security.transport.ssl.keystore.secure_password
+echo ""
+
+echo ""
+echo -e "y\n\n" | sudo /usr/share/elasticsearch/bin/elasticsearch-keystore add xpack.security.transport.ssl.truststore.secure_password
+echo ""
+
+# Reset the elastic password
 echo -e "y\nelastic\nelastic\n" | sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -i -u elastic
 
+# Restart all services
 sudo systemctl restart elasticsearch && sudo systemctl restart kibana
 
-echo -e "\nConnect to http://localhost:5601 for the interface, use username and password from command output"
+echo -e "\nConnect to http://localhost:5601 for the interface, creds are elastic:elastic"
